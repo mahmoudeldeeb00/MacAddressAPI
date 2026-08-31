eb9e03ee826da928bda2523b0769f8401fa857b0
mahmoudeldeeb00 <ae244910@gmail.com>
2026-08-31 18:14:41 +0300

    Add project files.

---
+++ MacAddressAPI/Controllers/MacAddressController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace MacAddressAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MacAddressController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMacAddress()
+        {
+            var macAddress = NetworkInterface
+                .GetAllNetworkInterfaces()
+                .Where(x =>
+                    x.OperationalStatus == OperationalStatus.Up &&
+                    x.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .Select(x => x.GetPhysicalAddress().ToString())
+                .FirstOrDefault(x => !string.IsNullOrEmpty(x));
+
+            return Ok(new
+            {
+                macAddress
+            });
+        }
+    }
+}
