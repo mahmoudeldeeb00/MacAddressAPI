ef2f69040180abb3ee898b282c0c45090afa4c90
mahmoudeldeeb00 <ae244910@gmail.com>
2026-09-07 12:26:29 +0300

    update cors

--- MacAddressAPI/Program.cs
+++ MacAddressAPI/Program.cs
@@ -1,23 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+
+    .SetIsOriginAllowed(origin => true) // السماح بأي Origin
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials());
+    ;
+});
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
